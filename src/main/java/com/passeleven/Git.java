d0e579a1940cfddeace2e64059cf706019911587
passeleven <passeleven@163.com>
2023-03-26 18:27:40 +0800

    hot-fix commit2

--- src/main/java/com/passeleven/Git.java
+++ src/main/java/com/passeleven/Git.java
@@ -6,5 +6,6 @@ public class Git {
         System.out.println("hello,git1");
         System.out.println("hello,git2");
         System.out.println("hello,git4");
+        System.out.println("hello,git hot-fix test");
     }
 }
